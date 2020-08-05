62e6c2da27011228955622f900bd99385f207e4d
Sarah <S00038939@auk.edu.kw>
2020-08-05 18:36:54 +0300

    added update button

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize({
   username: "postgres",
-  password: "HiamTheStar@95",
+  password: "Munirali1",
   database: "notebook",
   dialect: "postgres",
   host: "localhost",
